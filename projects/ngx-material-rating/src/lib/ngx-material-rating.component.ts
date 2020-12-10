8a38610976089445d3ded61ddcc0ead4489a547d
Fabian Vilers <fabian.vilers@dev-one.com>
2020-12-10 10:04:38 +0100

    Add two-way binding

--- projects/ngx-material-rating/src/lib/ngx-material-rating.component.ts
+++ projects/ngx-material-rating/src/lib/ngx-material-rating.component.ts
@@ -1,22 +1,72 @@
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import { Component, Input } from '@angular/core';
+import {
+  coerceBooleanProperty,
+  coerceNumberProperty,
+} from '@angular/cdk/coercion';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  forwardRef,
+  Input,
+  Output,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ThemePalette } from '@angular/material/core';
 
-type Stars = 'star' | 'star_border';
+export const NGX_MATERIAL_RATING_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => NgxMaterialRatingComponent),
+  multi: true,
+};
 
+export interface NgxMaterialRatingChange {
+  source: NgxMaterialRatingComponent;
+  value: number | null;
+}
+
+type Stars = 'star' | 'star_border';
 @Component({
   selector: 'ngx-material-rating',
+  exportAs: 'ngxMaterialRating',
+  providers: [NGX_MATERIAL_RATING_VALUE_ACCESSOR],
   templateUrl: './ngx-material-rating.component.html',
 })
-export class NgxMaterialRatingComponent {
+export class NgxMaterialRatingComponent implements ControlValueAccessor {
   @Input()
   color: ThemePalette = undefined;
 
   @Input()
-  max: number = 5;
+  get max(): number {
+    return this._max;
+  }
+  set max(v: number) {
+    this._max = coerceNumberProperty(v, this._max);
+    this._changeDetectorRef.markForCheck();
+  }
+  private _max: number = 5;
 
   @Input()
-  value: number = 0;
+  get value(): number | null {
+    if (this._value === null) {
+      this.value = 0;
+    }
+
+    return this._value;
+  }
+  set value(v: number | null) {
+    if (v !== this._value) {
+      let value = coerceNumberProperty(v);
+
+      if (this._roundToDecimal && value !== 0 && value !== this.max) {
+        value = parseFloat(value.toFixed(this._roundToDecimal));
+      }
+
+      this._value = value;
+      this._changeDetectorRef.markForCheck();
+    }
+  }
+  private _value: number | null = null;
+  private _roundToDecimal: number = 1;
 
   @Input()
   get disabled(): boolean {
@@ -27,6 +77,43 @@ export class NgxMaterialRatingComponent {
   }
   private _disabled: boolean = false;
 
+  @Output()
+  readonly change: EventEmitter<NgxMaterialRatingChange> = new EventEmitter<NgxMaterialRatingChange>();
+  readonly valueChange: EventEmitter<number | null> = new EventEmitter<
+    number | null
+  >();
+
+  onTouched: () => any = () => {};
+  private _cvaChangeFn: (value: any) => void = () => {};
+
+  constructor(private _changeDetectorRef: ChangeDetectorRef) {}
+
+  writeValue(value: any) {
+    this.value = value;
+  }
+
+  registerOnChange(fn: (value: any) => void) {
+    this._cvaChangeFn = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean) {
+    this.disabled = isDisabled;
+  }
+
+  onClick(index: number) {
+    if (this.disabled) {
+      return;
+    }
+
+    const newValue = index + 1;
+    this.value = this.value === newValue ? index : newValue;
+    this._emitChangeEvent();
+  }
+
   getStar(index: number): Stars {
     const value = this.value || 0;
 
@@ -35,5 +122,11 @@ export class NgxMaterialRatingComponent {
     }
 
     return 'star_border';
+  }
+
+  private _emitChangeEvent() {
+    this._cvaChangeFn(this.value);
+    this.valueChange.emit(this.value);
+    this.change.emit({ source: this, value: this.value });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     NgxMaterialRatingModule,
